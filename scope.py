e8b9b5b5ce2b162764a38b094bca45ad4644f540
xuqcf <sevt33nclothingbrand@gmail.com>
2026-01-11 16:57:01 -0500

    Scope.py

---
+++ scope.py
@@ -0,0 +1,14 @@
+modifier = 5
+level = 10
+
+def get_max_health(modifier, level):
+    return modifier * level
+
+
+## don't touch above this line
+
+max_health = get_max_health(modifier, level)
+
+# don't touch below this line
+
+print(f"max_health is: {max_health}")
