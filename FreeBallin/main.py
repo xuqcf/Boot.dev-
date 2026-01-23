27fe96875019f0b4b209b47847ea64d1a7b84061
xuqcf <sevt33nclothingbrand@gmail.com>
2026-01-23 07:51:31 -0500

    Square Calculation

--- FreeBallin/main.py
+++ FreeBallin/main.py
@@ -1,31 +1,16 @@
-class Unit:
-    def __init__(self, name, pos_x, pos_y):
-        self.name = name
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-    def in_area(self, x_1, y_1, x_2, y_2):
-        if x_1 <= self.pos_x <= x_2 and y_1 <= self.pos_y <= y_2:
-            return True
-        else:
-            return False
+    def get_area(self):
+        return self.length * self.width
+
+    def get_perimeter(self):
+        return 2 * (self.length + self.width)
 
 
-class Dragon(Unit):
-    def __init__(self, name, pos_x, pos_y, fire_range):
-        super().__init__(name, pos_x, pos_y)
-        self.__fire_range = fire_range
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
 
-    def breathe_fire(self, x, y, units):
-        hit_units = []
-
-        left   = x - self.__fire_range
-        right  = x + self.__fire_range
-        bottom = y - self.__fire_range
-        top    = y + self.__fire_range
-
-        for unit in units:
-            if unit.in_area(left, bottom, right, top):
-                hit_units.append(unit)
-            
-        return hit_units
--- FreeBallin/main_test.py
+++ FreeBallin/main_test.py
@@ -1,73 +1,42 @@
 from main import *
 
 run_cases = [
-    (
-        [Unit("Cian", 3, 3), Unit("Andrew", -1, 4), Unit("Baran", -6, 5)],
-        Dragon("Draco", 2, 2, 3),
-        2,
-        3,
-        ["Cian", "Andrew"],
-    ),
+    ((2, 4), 8, 12),
+    ((5,), 25, 20),
 ]
 
 submit_cases = run_cases + [
-    (
-        [
-            Unit("Carbry", 2, 1),
-            Unit("Yvor", 1, 0),
-            Unit("Eoin", 2, 0),
-            Unit("Edwin", 10, 10),
-        ],
-        Dragon("Fafnir", 1, 1, 1),
-        1,
-        1,
-        ["Carbry", "Yvor", "Eoin"],
-    ),
-    (
-        [Unit("Nicholas", 0, 1), Unit("Andrew", -1, 4), Unit("Baran", -6, 5)],
-        Dragon("Hydra", 0, 0, 2),
-        0,
-        1,
-        ["Nicholas"],
-    ),
-    (
-        [
-            Unit("Yvor", 1, 0),
-            Unit("Nicholas", 0, 1),
-            Unit("Eoin", 2, 0),
-            Unit("Cian", 3, 3),
-            Unit("Andrew", -1, 4),
-            Unit("Baran", -6, 5),
-            Unit("Carbry", 2, 1),
-        ],
-        Dragon("Smaug", 6, 6, 2),
-        1,
-        1,
-        ["Yvor", "Nicholas", "Eoin", "Cian", "Carbry"],
-    ),
+    ((1, 1), 1, 4),
+    ((3, 4), 12, 14),
+    ((6, 7), 42, 26),
+    ((8,), 64, 32),
+    ((9, 10), 90, 38),
 ]
 
 
-def test(units, dragon, x_target, y_target, expected_hit_units):
+def test(inputs, expected_area, expected_perimeter):
     print("---------------------------------")
-    print(f"{dragon.name} breathes fire at ({x_target}, {y_target})")
-    for unit in units:
-        print(f"  - {unit.name} is at ({unit.pos_x}, {unit.pos_y})")
-    print("")
-    print("Expecting to hit:")
-    for unit in expected_hit_units:
-        print(f"  - {unit}")
-    hit_units = dragon.breathe_fire(x_target, y_target, units)
-    hit_unit_names = [unit.name for unit in hit_units]
-    print("Actually hit:")
-    for unit in hit_units:
-        print(f"  - {unit.name}")
-    if set(hit_unit_names) == set(expected_hit_units):
-        print("Pass")
-        return True
-    else:
+    if len(inputs) == 2:  # Rectangle
+        shape = Rectangle(*inputs)
+        shape_type = "Rectangle"
+    else:  # Square
+        shape = Square(inputs[0])
+        shape_type = "Square"
+
+    print(f"Testing {shape_type} with inputs {inputs}")
+    area = shape.get_area()
+    perimeter = shape.get_perimeter()
+    print(f"Expected area: {expected_area}")
+    print(f"Actual area:   {area}")
+    print(f"Expected perimeter: {expected_perimeter}")
+    print(f"Actual perimeter:   {perimeter}")
+
+    if area != expected_area or perimeter != expected_perimeter:
         print("Fail")
         return False
+    else:
+        print("Pass")
+        return True
 
 
 def main():
