263c1054d91fa005887904158f78e7fd28491338
xuqcf <sevt33nclothingbrand@gmail.com>
2026-01-26 00:30:00 -0500

    correct

--- FreeBallin/main.py
+++ FreeBallin/main.py
@@ -1,13 +1,51 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x1, y1, x2, y2):
+        return (
+            self.pos_x >= x1
+            and self.pos_x <= x2
+            and self.pos_y >= y1
+            and self.pos_y <= y2
+        )
+
+
+# don't touch above this line
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, height, width, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.height = height
+        self.width = width
+        self.fire_range = fire_range
+
+        x1 = pos_x - width / 2
+        y1 = pos_y - height / 2
+        x2 = pos_x - width / 2
+        y2 = pos_y - height / 2
+
+        self.__hit_box = Rectangle(x1, y1, x2, y2)
+
+    def in_area(self, x1, y1, x2, y2):
+        area_rec = Rectangle(x1, y1, x2, y2)
+        return self.__hit_box.overlaps(area_rec)
+
+
+# don't touch below this line
+
+
 class Rectangle:
     def overlaps(self, rect):
-        return (self.get_left_x() <= rect.get_right_x() and
-                self.get_right_x() >= rect.get_left_x() and
-                self.get_top_y() >= rect.get_bottom_y() and
-                self.get_bottom_y() <= rect.get_top_y()
+        return (
+            self.get_left_x() <= rect.get_right_x()
+            and self.get_right_x() >= rect.get_left_x()
+            and self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
         )
-            
-
-    # don't touch below this line
 
     def __init__(self, x1, y1, x2, y2):
         self.__x1 = x1
@@ -34,6 +72,3 @@ class Rectangle:
         if self.__y1 < self.__y2:
             return self.__y1
         return self.__y2
-
-    def __repr__(self):
-        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
