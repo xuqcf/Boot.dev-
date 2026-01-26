281231aeb913d2ac21a6db40c7602b506eba6e1a
xuqcf <sevt33nclothingbrand@gmail.com>
2026-01-26 00:17:24 -0500

    Update

--- FreeBallin/main.py
+++ FreeBallin/main.py
@@ -1,4 +1,14 @@
 class Rectangle:
+    def overlaps(self, rect):
+        return (self.get_left_x() <= rect.get_right_x() and
+                self.get_right_x() >= rect.get_left_x() and
+                self.get_top_y() >= rect.get_bottom_y() and
+                self.get_bottom_y() <= rect.get_top_y()
+        )
+            
+
+    # don't touch below this line
+
     def __init__(self, x1, y1, x2, y2):
         self.__x1 = x1
         self.__y1 = y1
@@ -6,19 +16,24 @@ class Rectangle:
         self.__y2 = y2
 
     def get_left_x(self):
-        return min(self.__x1, self.__x2)
+        if self.__x1 < self.__x2:
+            return self.__x1
+        return self.__x2
 
     def get_right_x(self):
-        
-        return max(self.__x1, self.__x2)
+        if self.__x1 > self.__x2:
+            return self.__x1
+        return self.__x2
 
     def get_top_y(self):
-        return max(self.__y1, self.__y2)
+        if self.__y1 > self.__y2:
+            return self.__y1
+        return self.__y2
 
     def get_bottom_y(self):
-        return min(self.__y1, self.__y2)
-
-    # don't touch below this line
+        if self.__y1 < self.__y2:
+            return self.__y1
+        return self.__y2
 
     def __repr__(self):
         return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
--- FreeBallin/main_test.py
+++ FreeBallin/main_test.py
@@ -1,47 +1,36 @@
 from main import *
 
 run_cases = [
-    ((1, 2, 3, 4), (1, 3, 4, 2)),
-    ((3, 4, 1, 2), (1, 3, 4, 2)),
+    (Rectangle(0, 0, 4, 4), Rectangle(3, 3, 6, 6), True),
+    (Rectangle(0, 0, 4, 4), Rectangle(5, 5, 8, 8), False),
 ]
 
 submit_cases = run_cases + [
-    ((5, 4, 2, 1), (2, 5, 4, 1)),
+    (Rectangle(0, 0, 1, 1), Rectangle(4, 4, 5, 5), False),
+    (Rectangle(1, 1, 4, 4), Rectangle(2, 2, 3, 3), True),
+    (Rectangle(1, 1, 2, 2), Rectangle(0, 0, 4, 4), True),
+    (Rectangle(1, 1, 4, 4), Rectangle(1, 1, 4, 4), True),
+    (Rectangle(0, 0, 4, 1), Rectangle(1, 2, 3, 3), False),
 ]
 
 
-def test(rect_args, expected_output):
-    rectangle = Rectangle(rect_args[0], rect_args[1], rect_args[2], rect_args[3])
+def test(rect1, rect2, expected_overlap):
     print("---------------------------------")
-    print("Inputs Rectangle:")
-    print(f" * x1: {rect_args[0]}")
-    print(f" * y1: {rect_args[1]}")
-    print(f" * x2: {rect_args[2]}")
-    print(f" * y2: {rect_args[3]}")
+    print(f"Checking overlap of:")
+    print(f" - {rect1}")
+    print(f" - {rect2}")
     print("")
+    print(f"Expected overlap: {expected_overlap}")
 
-    expected_left_x, expected_right_x, expected_top_y, expected_bottom_y = (
-        expected_output
-    )
+    result = rect1.overlaps(rect2)
+    print(f"Actual overlap:   {result}")
 
-    actual_left_x = rectangle.get_left_x()
-    actual_right_x = rectangle.get_right_x()
-    actual_top_y = rectangle.get_top_y()
-    actual_bottom_y = rectangle.get_bottom_y()
-
-    print(f"Expected left x: {expected_left_x}")
-    print(f"Actual   left x: {actual_left_x}")
-    print(f"Expected right x: {expected_right_x}")
-    print(f"Actual   right x: {actual_right_x}")
-    print(f"Expected top y: {expected_top_y}")
-    print(f"Actual   top y: {actual_top_y}")
-    print(f"Expected bottom y: {expected_bottom_y}")
-    print(f"Actual   bottom y: {actual_bottom_y}")
-
-    result = (actual_left_x, actual_right_x, actual_top_y, actual_bottom_y)
-    if result == expected_output:
+    if result == expected_overlap:
+        print("Pass")
         return True
-    return False
+    else:
+        print("Fail")
+        return False
 
 
 def main():
@@ -51,10 +40,8 @@ def main():
     for test_case in test_cases:
         correct = test(*test_case)
         if correct:
-            print("Pass")
             passed += 1
         else:
-            print("Fail")
             failed += 1
     if failed == 0:
         print("============= PASS ==============")
