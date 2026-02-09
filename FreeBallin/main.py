3149fb8c4fd0569f3557d3c368d6fe76ce056aa4
xuqcf <sevt33nclothingbrand@gmail.com>
2026-02-08 21:17:57 -0500

    Enums

--- FreeBallin/main.py
+++ FreeBallin/main.py
@@ -1,20 +1,4 @@
-class MaybeParsed:
-    pass
+from enum import Enum
 
+Doctype = Enum('Doctype', ['PDF', 'TXT', 'DOCX', 'MD', 'HTML'])
 
-# don't touch above this line
-
-
-class Parsed(MaybeParsed):
-    def __init__(self, doc_name, text):
-        self.doc_name = doc_name
-        self.text = text
-
-
-
-
-
-class ParseError(MaybeParsed):
-    def __init__(self, doc_name, err):
-        self.doc_name = doc_name
-        self.err = err
--- FreeBallin/main_test.py
+++ FreeBallin/main_test.py
@@ -1,41 +1,28 @@
 from main import *
 
 run_cases = [
-    Parsed("why_fp.txt", "Because we're better than everyone else"),
-    ParseError("why_fp.docx", "Can't handle weird windows files"),
+    (lambda: Doctype.PDF, "Doctype.PDF", False),
+    (lambda: Doctype.TXT, "Doctype.TXT", False),
+    (lambda: Doctype.DOCX, "Doctype.DOCX", False),
+    (lambda: Doctype.MD, "Doctype.MD", False),
 ]
 
 submit_cases = run_cases + [
-    Parsed("why_fp.md", "Because we're better than everyone else"),
-    ParseError("why_fp.pdf", "Can't handle weird adobe files"),
+    (lambda: Doctype.HTML, "Doctype.HTML", False),
+    (lambda: Doctype.Invalid, "Doctype.Invalid", True),
 ]
 
 
-def test(obj):
+def test(func, name, is_err):
     print("---------------------------------")
-    print(f"Testing properties of {obj.doc_name}...")
-    if isinstance(obj, Parsed):
-        if not obj.text:
-            print(f"Expecting .text to be non-empty")
-            print("Fail")
-            return False
-        if not obj.doc_name:
-            print(f"Expecting .doc_name to be non-empty")
-            print("Fail")
-            return False
-    elif isinstance(obj, ParseError):
-        if not obj.err:
-            print(f"Expecting .err to be non-empty")
-            print("Fail")
-            return False
-        if not obj.doc_name:
-            print(f"Expecting .doc_name to be non-empty")
-            print("Fail")
-            return False
-    else:
-        raise ValueError(f"unknown class type for: {obj}")
-    print("Pass")
-    return True
+    print(f"Checking value: {name}")
+    try:
+        val = func()
+        print(f"...Valid enum value!")
+        return not is_err
+    except Exception as e:
+        print(f"...Invalid enum value!")
+        return is_err
 
 
 def main():
@@ -43,10 +30,12 @@ def main():
     failed = 0
     skipped = len(submit_cases) - len(test_cases)
     for test_case in test_cases:
-        correct = test(test_case)
+        correct = test(*test_case)
         if correct:
+            print("Pass")
             passed += 1
         else:
+            print("Fail")
             failed += 1
     if failed == 0:
         print("============= PASS ==============")
