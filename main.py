fd21eef43259351bd0eebfd18b63abaf123fb9d8
xuqcf <sevt33nclothingbrand@gmail.com>
2026-04-11 20:24:51 -0400

    bubble sort

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def bubble_sort(nums):
         swapping = False
         for i in range(1, end):
             if nums[i - 1] > nums[i]:
-                nums[ i -1 ], nums[i] = nums[i], nums[ i - 1]
+                nums[i -1], nums[i] = nums[i], nums[i - 1]
                 swapping = True
         end -= 1
     return nums
